3ce3d082dee2bbaf7a080ce5cd61675264a3c0c0
martkir <martinbarut@hotmail.com>
2020-03-25 15:40:40 +0000

    added histogram plots

--- eval.py
+++ eval.py
@@ -8,8 +8,6 @@ import click
 from tabulate import tabulate
 
 """
-
-
 tasks:
 - change a training evaluation job to update the "index" file.
 - modify TrainEvalJob to create index if it doesn't exist.
@@ -39,17 +37,6 @@ def get_job_str(job_config):
     return ' '.join(['{}:{}'.format(k, v) for k, v in job_config.items()])
 
 
-def jobs_histogram(jobs_ids, metric, bins, fig_path):
-    jobs_summary_df = summarize_jobs(jobs_ids, metric)
-    if metric in jobs_summary_df:
-        vals = np.array(jobs_summary_df[metric])
-        plt.hist(vals, bins=bins)
-        plt.savefig(fig_path)
-    else:
-        raise Exception('metric {} is not a column in the results of the specified jobs.')
-    print('Finished creating figure. Saved at {}'.format(fig_path))
-
-
 def run_jobs():
     import mf
 
@@ -66,14 +53,30 @@ def run_jobs():
 
 
 class EvalHist(object):
-    pass
+    def __init__(self, schema_path, metric, index_path, bins):
+        self.schema_path = schema_path
+        self.metric = metric
+        self.index_path = index_path
+        self.bins = bins
+        self.table = EvalTable(
+            schema_path=self.schema_path,
+            agg_option='all',
+            metric=self.metric,
+            index_path=self.index_path
+        )
+        self.table.create()
+        self.vals = np.array(self.table.table[self.metric])
+
+    def save(self, hist_path):
+        plt.hist(self.vals, bins=self.bins)
+        plt.savefig(hist_path)
+
 
 class EvalTable(object):
-    def __init__(self, schema_path, agg_option, metric, table_path, index_path):
+    def __init__(self, schema_path, agg_option, metric, index_path):
         self.schema_path = schema_path
         self.agg_option = agg_option
         self.metric = metric
-        self.table_path = table_path
         self.index_path = index_path
         self.schema = json.load(open(self.schema_path, 'r'))
         self.index = json.load(open(self.index_path, 'r'))
@@ -102,6 +105,9 @@ class EvalTable(object):
         print('Present jobs:')
         for job_str in present_jobs:
             print(job_str)
+
+        # just ask to run these jobs in order to continue.
+
         print('Missing jobs:')
         for job_str in missing_jobs:
             print(job_str)
@@ -138,31 +144,40 @@ class EvalTable(object):
         else:
             self.table = self.best()
 
-    def save(self):
-        with open(self.table_path, 'w+') as file:
+    def save(self, table_path):
+        with open(table_path, 'w+') as file:
             table_str = tabulate(self.table, tablefmt="pipe", headers="keys", showindex=False)
             file.write(table_str)
 
 
 @click.command()
-@click.option('--compare', nargs=3, type=(str, str, str), default=('schemas/test.json', 'best', 'table_best.md'))
+@click.option('--compare', type=str)  # options: all, best.
+@click.option('--hist', type=int)
 @click.option('--metric', type=str, default='valid_mse')
-@click.option('--index_path', default='jobs/index.json')
-@click.option('--hist', type=bool)
-def main(compare, metric, index_path, hist):
+@click.option('--schema', type=str)
+@click.option('--index', type=str, default='jobs/index.json')
+@click.option('--save', type=str)
+def main(compare, hist, metric, schema, index, save):
     if compare:
         table = EvalTable(
-            schema_path=compare[0],
-            agg_option=compare[1],
+            schema_path=schema,
+            agg_option=compare,
             metric=metric,
-            table_path=compare[2],
-            index_path=index_path
+            index_path=index
         )
         table.create()
-        table.save()
+        table.save(table_path=save)
 
     if hist:
-        print('todo')
+        print('entered hist')
+        plot = EvalHist(
+            schema_path=schema,
+            metric=metric,
+            index_path=index,
+            bins=hist
+        )
+        plot.save(hist_path=save)
+
 
 if __name__ == '__main__':
     main()
